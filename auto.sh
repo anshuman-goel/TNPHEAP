75d2cc52c43c28a6d1be4bffbc81144c5fa14342
Zubin Thampi <zsthampi@ncsu.edu>
2017-10-05 19:18:00 -0400

    Updated auto.sh and gitignore

--- auto.sh
+++ auto.sh
@@ -3,7 +3,7 @@ sudo rmmod tnpheap
 cd kernel_module
 make
 sudo make install
-sudo insmod NPHeap/npheap.ko
+sudo insmod ../NPHeap/npheap.ko
 sudo chmod 777 /dev/npheap
 sudo insmod tnpheap.ko
 sudo chmod 777 /dev/tnpheap

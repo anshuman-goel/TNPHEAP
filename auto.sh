69c50791bee9aa9efb32756004ace0140eea6a69
zsthampi <zsthampi@ncsu.edu>
2017-10-05 18:14:47 -0400

    Added auto.sh and updated benchmark

---
+++ auto.sh
@@ -0,0 +1,14 @@
+sudo rmmod npheap
+sudo rmmod tnpheap
+cd kernel_module
+make
+sudo make install
+sudo insmod NPHeap/npheap.ko
+sudo chmod 777 /dev/npheap
+sudo insmod tnpheap.ko
+sudo chmod 777 /dev/tnpheap
+cd ../library
+make
+sudo make install
+cd ../benchmark
+make
